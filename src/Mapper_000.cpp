b701236838d7b83821bf574f74536b9f32600d42
MichailTalReborn <mikhailtal60@proton.me>
2026-04-07 19:09:50 +0200

    Even more bug fixing

--- src/Mapper_000.cpp
+++ src/Mapper_000.cpp
@@ -1,17 +1,18 @@
 #include "Mapper_000.h"
-#include <cstdint>
 
 Mapper_000::Mapper_000(uint8_t prgBanks, uint8_t chrBanks)
     : Mapper(prgBanks, chrBanks) {}
 
 Mapper_000::~Mapper_000() {}
 
-bool Mapper_000::cpuMapRead(uint16_t addr, uint32_t &mapped_addr) {
+// void Mapper_000::reset() {}
 
+bool Mapper_000::cpuMapRead(uint16_t addr, uint32_t &mapped_addr) {
   if (addr >= 0x8000 && addr <= 0xFFFF) {
     mapped_addr = addr & (nPRGBanks > 1 ? 0x7FFF : 0x3FFF);
     return true;
   }
+
   return false;
 }
 
@@ -21,6 +22,7 @@ bool Mapper_000::cpuMapWrite(uint16_t addr, uint32_t &mapped_addr,
     mapped_addr = addr & (nPRGBanks > 1 ? 0x7FFF : 0x3FFF);
     return true;
   }
+
   return false;
 }
 
@@ -29,6 +31,7 @@ bool Mapper_000::ppuMapRead(uint16_t addr, uint32_t &mapped_addr) {
     mapped_addr = addr;
     return true;
   }
+
   return false;
 }
 
@@ -40,5 +43,6 @@ bool Mapper_000::ppuMapWrite(uint16_t addr, uint32_t &mapped_addr) {
       return true;
     }
   }
+
   return false;
 }
